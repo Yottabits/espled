1d2c1818f365efef518429dea253d402517b7ed0
Dominik Kogel <dominik.kogel@djk96.rocks>
2020-04-15 19:16:50 +0200

    add type Definitions according to our needs

--- software/lib/Definitions/TypeDefinitions.h
+++ software/lib/Definitions/TypeDefinitions.h
@@ -1,11 +1,25 @@
 #ifndef TYPEDEFINITIONS_H
 #define TYPEDEFINITIONS_H
 
-typedef struct {
-  unsigned int R, G, B, CW, WW;
-} CRGBWW;
+//Struct for Storing Color Values
+class CRGBWW{
+  public:
+    unsigned int R, G, B, CW, WW;
+
+    //operator overloading
+    //todo put this into an c++ file
+    bool operator==(const CRGBWW &color) const{
+      return (this->R == color.R && 
+          this->G == color.G &&  
+          this->B == color.B && 
+          this->CW == color.CW && 
+          this->WW == color.WW);
+    }
+};
 
 
+//Variable Silo -> Datastructure that gets recived via MQTT and defines what the 
+//Annimation Handlers/Modes do
 typedef struct{
   unsigned char mode;
   CRGBWW colorValue;
@@ -16,5 +30,11 @@ typedef struct{
   unsigned int length;
 } varSilo;
 
+//Supported LED Strip Types
 enum stripType {RGB = 3, RGBW, RGBWW, WS2812, APA102};
+
+
+//LED Strip Modes
+enum Mode {FADE_2_COLOR, BLINK_COLOR, STROBE, SOUND_2_LIGHT, FIRE, NOISE, VU_METER, STROBE_PARTS, MOVING_PARTS};
+
 #endif
