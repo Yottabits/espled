88413942f215d4b1b9818f11d61016241fef8cdd
Dominik Kogel <dominik.kogel@djk96.rocks>
2020-04-15 11:47:48 +0200

    add footprint of handler function

--- software/lib/AnimationHandlerBus/AnimationHandlerBus.cpp
+++ software/lib/AnimationHandlerBus/AnimationHandlerBus.cpp
@@ -1,5 +1,19 @@
 #include "AnimationHandlerBus.h"
 
 void AnimationHandlerBus::fade(CRGBWW value, double transitionTime){
-
+//case FIRE:
+//        fire();
+//        break;
+//    case NOISE:
+//        noise();
+//        break;
+//    case VU_METER:
+//        vuMeter():
+//        break;
+//    case STROBE_PARTS:
+//        strobeParts();
+//        break;
+//    case MOVING_PARTS:
+//        movingParts();     
+//        break;
 }
