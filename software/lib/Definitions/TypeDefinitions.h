b5ef506302d5d896abd3ce33eb2669d408c1570a
domiko96 <dominik.kogel@djk96.rocks>
2020-10-24 00:29:36 +0200

    reformat

--- software/lib/Definitions/TypeDefinitions.h
+++ software/lib/Definitions/TypeDefinitions.h
@@ -2,25 +2,27 @@
 #define TYPEDEFINITIONS_H
 
 //Struct for Storing Color Values
-class CRGBWW{
-  public:
-    unsigned int R, G, B, WW, CW;
+class CRGBWW
+{
+public:
+  unsigned int R, G, B, WW, CW;
 
-    //operator overloading
-    //todo put this into an c++ file
-    bool operator==(const CRGBWW &color) const{
-      return (this->R == color.R &&
-          this->G == color.G &&
-          this->B == color.B &&
-          this->CW == color.CW &&
-          this->WW == color.WW);
-    }
+  //operator overloading
+  //todo put this into an c++ file
+  bool operator==(const CRGBWW &color) const
+  {
+    return (this->R == color.R &&
+            this->G == color.G &&
+            this->B == color.B &&
+            this->CW == color.CW &&
+            this->WW == color.WW);
+  }
 };
-
 
 //Variable Silo -> Datastructure that gets recived via MQTT and defines what the
 //Annimation Handlers/Modes do
-typedef struct varSilo{
+typedef struct varSilo
+{
   unsigned char mode;
   CRGBWW colorValue;
   unsigned int time;
@@ -33,20 +35,48 @@ typedef struct varSilo{
   unsigned int timeVariance;
   unsigned int maxBrightnesVariance;
   unsigned int duration;
-  varSilo* oldVarSilo;
+  varSilo *oldVarSilo;
   CRGBWW oldColor;
   unsigned long lastChange;
 } varSilo;
 
 //Supported LED Strip Types
-enum stripType {RGB_STRIP = 3, RGBW_STRIP, RGBWW_STRIP, WS2812_STRIP, APA102_STRIP};
-
+enum stripType
+{
+  RGB_STRIP = 3,
+  RGBW_STRIP,
+  RGBWW_STRIP,
+  WS2812_STRIP,
+  APA102_STRIP
+};
 
 //LED Strip Modes
-enum Mode {FADE_2_COLOR, BLINK_COLOR, STROBE, SOUND_2_LIGHT, BREATHE, TEMPVIS, INDUSTRIAL_FLICKER, FIRE = 101, NOISE, VU_METER, STROBE_PARTS, MOVING_PARTS};
+enum Mode
+{
+  FADE_2_COLOR,
+  BLINK_COLOR,
+  STROBE,
+  SOUND_2_LIGHT,
+  BREATHE,
+  TEMPVIS,
+  INDUSTRIAL_FLICKER,
 
+  WIPE = 100,
+  FIRE,
+  NOISE,
+  VU_METER,
+  STROBE_PARTS,
+  MOVING_PARTS
+};
 
 //Log LEVELS
-enum LogLevel {VERBOSE, DEBUG, INFO, WARNING, ERROR};
+enum LogLevel
+{
+  VERBOSE,
+  DEBUG,
+  INFO,
+  WARNING,
+  ERROR
+};
 
 #endif
