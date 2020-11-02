54c36abb3ca3efec8e12398a02b9eb2782d1d54c
Andre Eberhard <andre-eberhard@freenet.de>
2020-11-02 16:48:14 +0100

    Add position to vuMeter

--- software/lib/AnimationHandlerBus/AdressableModes/vuMeter.cpp
+++ software/lib/AnimationHandlerBus/AdressableModes/vuMeter.cpp
@@ -10,25 +10,52 @@ void AnimationHandlerBus::vuMeter()
     static unsigned int maxAmplitudeLed = 0;
     static unsigned int fourierTimer = 0;
 
-
     unsigned int numberOfLedsToTurnOn = (micHandler->getAverageAmplitude() * (1 + (silo->sensitivity * 0.09))) * this->stripLength;
+
     if(numberOfLedsToTurnOn > this->stripLength)
       numberOfLedsToTurnOn = this->stripLength;
+
+    CRGB color = CRGBWW2FastLedCRGB(silo->colorValue);
 
     if(numberOfLedsToTurnOn > maxAmplitudeLed)
       maxAmplitudeLed = numberOfLedsToTurnOn;
 
-    CRGB color = CRGBWW2FastLedCRGB(silo->colorValue);
-    //static CRGB color = CRGB::Black;
+    switch(silo->position){
+      case 0:
+        for(unsigned int i = numberOfLedsToTurnOn; i < this->stripLength; i++){
+          this->leds[i] = CRGB::Black;
+        }
+        for(unsigned int i = 0; i<numberOfLedsToTurnOn; i++){
+          this->leds[i] = color;
+        }
+        this->leds[maxAmplitudeLed] = CRGB::Red;
+        break;
 
-    for(unsigned int i = 0; i<numberOfLedsToTurnOn; i++){
-      this->leds[i] = color;
-    }
-    for(unsigned int i = numberOfLedsToTurnOn; i < this->stripLength; i++){
-      this->leds[i] = CRGB::Black;
+      case 1:
+        numberOfLedsToTurnOn /= 2;
+        for(unsigned int i = 0; i < this->stripLength-numberOfLedsToTurnOn; i++){
+          this->leds[i] = CRGB::Black;
+          this->leds[this->stripLength-i] = CRGB::Black;
+        }
+        for(unsigned int i = 0; i<numberOfLedsToTurnOn; i++){
+          this->leds[(this->stripLength/2)+i] = color;
+          this->leds[(this->stripLength/2)-i] = color;
+        }
+        this->leds[(this->stripLength/2)+maxAmplitudeLed/2] = CRGB::Red;
+        this->leds[(this->stripLength/2)-maxAmplitudeLed/2] = CRGB::Red;
+        break;
+
+      case 2:
+        for(unsigned int i = 0; i < this->stripLength-numberOfLedsToTurnOn; i++){
+          this->leds[i] = CRGB::Black;
+        }
+        for(unsigned int i = this->stripLength; i>this->stripLength-numberOfLedsToTurnOn; i--){
+          this->leds[i] = color;
+        }
+        this->leds[this->stripLength-maxAmplitudeLed] = CRGB::Red;
+        break;
     }
 
-    this->leds[maxAmplitudeLed] = CRGB::Red;
 
     if(now - maxLedDownTimer > 50){
       maxLedDownTimer = now;
